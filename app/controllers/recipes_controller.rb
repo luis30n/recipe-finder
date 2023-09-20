622222e1ba495a2a3fb955275071a1a15d40f523
Luis Alvarez Lopez <luis30n@gmail.com>
2023-09-20 22:08:16 +0200

    [feature] Allow recipe search by any ingredient

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,7 +2,18 @@
 
 class RecipesController < ApplicationController
   def index
-    @recipes = Recipe.order(rating_average: :desc)
-    render json: @recipes
+    @recipes = find_recipes.paginate(page: params[:page])
+    respond_to do |format|
+      format.html
+      format.json { render json: @recipes }
+    end
+  end
+
+  private
+
+  def find_recipes
+    return Recipe.order(rating_average: :desc) if params[:ingredients].blank?
+
+    Recipe.with_any_ingredients(params[:ingredients])
   end
 end
